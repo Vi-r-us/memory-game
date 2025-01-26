96dbb98e25989d4cb2821b7baaa72c247ff18ba8
Sahil Kumar <sahil23012000@gmail.com>
2025-01-26 17:15:17 +0530

    basename changed for BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/memory-game/">
       <Routes>
         <Route path="/" element={<Home isLoading={isLoading} />} />
         <Route path="/game" element={<Game />} />
